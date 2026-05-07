9cabbc24fb0d007dcdebb2862500669441ba2287
JonahGalahad <jonycala23@gmail.com>
2026-05-07 00:01:53 -0300

    Actualizacion de funciones del projectService.js

--- src/services/proyectService.js
+++ src/services/proyectService.js
@@ -3,52 +3,31 @@ const proyectService = ( () => {
     // Array de proyectos para simular una base de datos
     let proyectos = [
         // Categorias: "En proceso", "Finalizado", "En espera", "Cancelado", "En revisión"
-        {
-            id: 1,
-            titulo: "Proyecto [1]",
-            categoria: "Categoría (1)",
-            estado: "En proceso"
-        },
-        {
-            id: 2,
-            titulo: "Proyecto [2]",
-            categoria: "Categoría (2)",
-            estado: "Finalizado"
-        },
-        {
-            id: 3,
-            titulo: "Proyecto [3]",
-            categoria: "Categoría (3)",
-            estado: "En espera"
-        },
-        {
-            id: 4,
-            titulo: "Proyecto [4]",
-            categoria: "Categoría (4)",
-            estado: "En revisión"
-        },
-        {
-            id: 5,
-            titulo: "Proyecto [5]",
-            categoria: "Categoría (5)",
-            estado: "Cancelado"
-        }
+        { id: 1, titulo: "Sistema de Gestión de Biblioteca", categoria: "Programación", estado: "En proceso"},
+        { id: 2, titulo: "Aplicación para Organización de Tareas", categoria: "Programación", estado: "Finalizado"},
+        { id: 3, titulo: "Sitio Web Institucional", categoria: "Diseño", estado: "En espera" },
+        { id: 4, titulo: "Registro de Alumnos", categoria: "Programación", estado: "En revisión" },
+        { id: 5, titulo: "Plataforma de Cursos Online", categoria: "Educación", estado: "Cancelado" }
     ];
 
     // Funciones para manejar los proyectos
     const obtenerProyectos = () => [...proyectos];//retorna una COPIA del array "proyectos" para evitar modificaciones externas
     
-    const eliminarProyecto = (id) => {
-        // el filter se utiliza para crear un nuevo array que excluye el proyecto con el id especificado
-        proyectos = proyectos.filter(proyecto => proyecto.id !== id);
+    // Funcion para recibir un objeto proyecto y lo agrega al array
+    const agregarProyecto = (proyecto) => {
+        const newID = Math.max(...proyectos.map(({id}) => id),0) + 1;
+        proyectos = [...proyectos, {...proyecto, id: newID}];
     };
 
-    const agregarProyecto = () => {
-
+    //Funcion para eliminar un objeto proyecto
+    const eliminarProyecto = (idBuscado) => {
+        // el filter se utiliza para crear un nuevo array que excluye el proyecto con el id especificado
+        proyectos = proyectos.filter(({id}) => id !== idBuscado);
     };
     
-    const buscarProyecto = () => {
-
+    // Funcion para buscar y retornar un proyecto basandose en el titulo
+    const buscarProyecto = (text) => {
+        return proyectos.filter(({titulo}) => titulo.toLowerCase().includes(text.toLowerCase()));
     };
 
     return {
