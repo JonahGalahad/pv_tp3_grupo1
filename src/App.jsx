df4c1709ff5f78e99bde754d1292a0d24c773e99
Vasquez Ariana <arianagustinavqz@gmail.com>
2026-06-08 19:29:42 -0300

    (feat tp4) configuracion de rutas, actualizar navegación y reorganizar vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from './assets/vite.svg'
 //import heroImg from './assets/hero.png'
 //import './App.css'
 import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import './css/styles.css'
 import './css/listaProyectos.css'
 import ListaProyectos from './components/ListaProyectos.jsx'
-import PerfilUsuario from './components/PerfilUsuario.jsx'
+import DetalleProyecto from './components/DetalleProyecto';
+import PerfilUsuario from "./views/PerfilUsuario";
 
 const App = () => {
-  return(
-    <div className="app-wrapper">
-      
-      <main className='content-area'>
-        <Navbar/>
-        <Header/>
-        <PerfilUsuario />
-        <ListaProyectos/>
-        <Footer/>
-      </main>
-      
+  return (
+    <BrowserRouter>
 
-    </div>
-    
-  )
-}
+      <div className="app-wrapper">
+
+        <Navbar />
+        <Header />
+
+        <main className="content-area">
+
+          <Routes>
+            <Route path="/" element={<ListaProyectos />} />
+            <Route path="/proyectos" element={<ListaProyectos />} />
+            <Route path="/perfil" element={<PerfilUsuario />} />
+            <Route path="/proyectos/:id" element={<DetalleProyecto />} />
+          </Routes>
+
+        </main>
+
+        <Footer />
+
+      </div>
+
+    </BrowserRouter>
+  );
+};
+
 export default App;
